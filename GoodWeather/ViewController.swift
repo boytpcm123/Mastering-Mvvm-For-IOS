136955086424d83f809884ae480f1cf7d4632151
Thong Nguyen <thong.nguyen@hdwebsoft.co>
2018-12-31 20:07:09 +0700

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by hungdat1234 on 12/31/18.
+//  Copyright © 2018 ninjaKID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
